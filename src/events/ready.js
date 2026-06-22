b3e6793092e57578bc5acd5d0dda9378561e92a7
페헤 <peheagma977@gmail.com>
2026-06-22 17:47:57 +0900

    봇 상태 메시지에 서버 수 표시
    서버에 참여/이탈할 때마다 갱신되는 커스텀 상태로 'N개의 서버와 함께하는 중이다냥' 표시

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,26 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
+const { nya } = require("../utils/nya");
+
+function updatePresence(client) {
+  client.user.setPresence({
+    status: "online",
+    activities: [
+      {
+        name: "status",
+        type: ActivityType.Custom,
+        state: nya(`${client.guilds.cache.size}개의 서버와 함께하는 중이다`),
+      },
+    ],
+  });
+}
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`${client.user.tag} 봇이 준비되었습니다.`);
+    updatePresence(client);
+    client.on(Events.GuildCreate, () => updatePresence(client));
+    client.on(Events.GuildDelete, () => updatePresence(client));
   },
 };
